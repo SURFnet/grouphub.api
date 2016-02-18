830e3b33c896555ed1b3eaf1ef8aabfe278e8850
Tjeerd Bijlsma <tjeerd@ibuildings.nl>
2016-02-18 23:39:06 +0100

    Added call to remove a Notification

--- src/AppBundle/Controller/UserNotificationController.php
+++ src/AppBundle/Controller/UserNotificationController.php
@@ -4,6 +4,7 @@ namespace AppBundle\Controller;
 
 use AppBundle\Entity\User;
 use FOS\RestBundle\Controller\FOSRestController;
+use FOS\RestBundle\View\View;
 use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
 
 /**
@@ -14,7 +15,7 @@ class UserNotificationController extends FOSRestController
     /**
      * @param int $id
      *
-     * @return array
+     * @return View
      */
     public function getUserNotificationsAction($id)
     {
@@ -22,11 +23,34 @@ class UserNotificationController extends FOSRestController
         $user = $this->getDoctrine()->getRepository('AppBundle:User')->find($id);
 
         if ($user === null) {
-            throw new NotFoundHttpException('User with id: '.$id.' not found');
+            throw new NotFoundHttpException('User with id: ' . $id . ' not found');
         }
 
         $notifications = $this->getDoctrine()->getRepository('AppBundle:Notification')->findBy(['to' => $user]);
 
         return $this->view($notifications);
+    }
+
+    /**
+     * @param int $userId
+     * @param int $id
+     *
+     * @return View
+     */
+    public function deleteUserNotificationsAction($userId, $id)
+    {
+        $notification = $this->getDoctrine()->getRepository('AppBundle:Notification')->findOneBy(
+            ['to' => $userId, 'id' => $id]
+        );
+
+        if ($notification === null) {
+            throw new NotFoundHttpException('Notification ' . $id . ' not found for user ' . $userId);
+        }
+
+        $em = $this->getDoctrine()->getManager();
+        $em->remove($notification);
+        $em->flush();
+
+        return $this->routeRedirectView('get_user_notifications', ['id' => $userId]);
     }
 }
