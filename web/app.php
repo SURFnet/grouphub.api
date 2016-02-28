1a491f8ec6b63e2b54ec54a3398dfbf995d3c22e
Tjeerd Bijlsma <tjeerd@ibuildings.nl>
2016-02-28 09:47:30 +0100

    Updated packages

--- web/app.php
+++ web/app.php
@@ -5,13 +5,8 @@ use Symfony\Component\HttpFoundation\Request;
 
 $loader = require_once __DIR__.'/../app/autoload.php';
 
-// Use APC for autoloading to improve performance.
-// Change 'sf2' to a unique prefix in order to prevent cache key conflicts
-// with other applications also using APC.
-/*
-$loader = new ApcClassLoader('sf2', $loader);
+$loader = new ApcClassLoader(sha1(__FILE__), $loader);
 $loader->register(true);
-*/
 
 require_once __DIR__.'/../app/AppKernel.php';
 //require_once __DIR__.'/../app/AppCache.php';
