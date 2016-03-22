0b4cf603ffd7a820f6c703abf3cde4d54b20b8cd
Tjeerd Bijlsma <tjeerd@ibuildings.nl>
2016-03-22 14:30:59 +0100

    Make use of dedicated deployment user

--- app/config/deployment/stages/staging.rb
+++ app/config/deployment/stages/staging.rb
@@ -1,4 +1,4 @@
-server '145.100.181.12', user: 'tjeerd', roles: %w{web db app}
+server '145.100.181.12', user: 'deployment', roles: %w{web db app}
 
 set :deploy_to, '/var/www/grouphub.api'
 set :file_permissions_users, ["www-data"]
