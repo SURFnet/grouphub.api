91330aad7c6ed989ab35e47bedb1b5313495571c
Tjeerd Bijlsma <tjeerd@ibuildings.nl>
2016-03-24 19:44:58 +0100

    Ask for tag during deployment

--- app/config/deployment/deploy.rb
+++ app/config/deployment/deploy.rb
@@ -5,7 +5,7 @@ set :application, 'grouphub.api'
 set :repo_url, 'git@github.com:SURFnet/grouphub.api.git'
 
 # Default branch is :master
-ask :branch, `git rev-parse --abbrev-ref HEAD`.chomp
+ask :branch, `git tag`.split("\n").last
 
 # Default deploy_to directory is /var/www/my_app_name
 set :deploy_to, '/var/www/grouphub.api'
