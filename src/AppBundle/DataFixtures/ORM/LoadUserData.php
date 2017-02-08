0f0dbfe35aedcaa629f76279b804e25cbeec9800
Nic Wortel <nic@nicwortel.nl>
2017-02-08 13:24:22 +0100

    Add an annotation to the user for development purposes

--- src/AppBundle/DataFixtures/ORM/LoadUserData.php
+++ src/AppBundle/DataFixtures/ORM/LoadUserData.php
@@ -3,6 +3,7 @@
 namespace AppBundle\DataFixtures\ORM;
 
 use AppBundle\Entity\User;
+use AppBundle\Entity\UserAnnotation;
 use Doctrine\Common\DataFixtures\AbstractFixture;
 use Doctrine\Common\DataFixtures\OrderedFixtureInterface;
 use Doctrine\Common\Persistence\ObjectManager;
@@ -81,6 +82,12 @@ class LoadUserData extends AbstractFixture implements OrderedFixtureInterface, C
         $user->setTimeStamp(new \DateTime());
         $user->setReference($this->container->getParameter('admin_dn'));
         $user->setType('ldap');
+
+        $annotation = new UserAnnotation();
+        $annotation->setAttribute('employeeNumber');
+        $annotation->setValue('1234');
+        $user->addAnnotation($annotation);
+
         $manager->persist($user);
 
         $this->addReference('admin-user', $user);
